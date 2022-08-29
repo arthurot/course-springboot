d4ae3f99a8238cfc63cee605406a8b12434ad616
Arthur Tavares <arthurotavares@gmail.com>
2022-08-29 18:41:30 -0300

    OrderStatus enum

---
+++ src/main/java/com/arthurdev/course/services/CategoryService.java
@@ -0,0 +1,28 @@
+package com.arthurdev.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.arthurdev.course.entities.Category;
+import com.arthurdev.course.repositories.CategoryRepository;
+
+@Service
+//registrado como componente do spring para a injecao de dependencia
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository repository;
+
+	public List<Category> findAll() {
+		return repository.findAll();
+	}
+
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
