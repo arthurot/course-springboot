fe9e23184325f29865896871d54e97a27a989b38
Arthur Tavares <arthurotavares@gmail.com>
2022-08-31 10:32:11 -0300

    Payment, one-to-one association

--- src/main/java/com/arthurdev/course/config/TesteConfig.java
+++ src/main/java/com/arthurdev/course/config/TesteConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.arthurdev.course.entities.Category;
 import com.arthurdev.course.entities.Order;
 import com.arthurdev.course.entities.OrderItem;
+import com.arthurdev.course.entities.Payment;
 import com.arthurdev.course.entities.Product;
 import com.arthurdev.course.entities.User;
 import com.arthurdev.course.entities.enums.OrderStatus;
@@ -81,6 +82,13 @@ public class TesteConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+	
+		Payment pay1 = new Payment(null,  Instant.parse("2022-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
+		
+	
 	}
+	
 
 }
--- src/main/java/com/arthurdev/course/entities/Order.java
+++ src/main/java/com/arthurdev/course/entities/Order.java
@@ -6,6 +6,7 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -13,11 +14,11 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import com.arthurdev.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "tb_order")
@@ -39,6 +40,10 @@ public class Order implements Serializable {
 	
 	@OneToMany(mappedBy = "id.order")
 	private Set<OrderItem> items = new HashSet<>();
+	
+	//mapeamento 1 para 1, o cascade eh para os dois terem os mesmo ID
+	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+	private Payment payment;
 
 	public Order() {
 	}
@@ -85,9 +90,19 @@ public class Order implements Serializable {
 		this.client = client;
 	}
 
+	
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
+	}
+
 	public Set<OrderItem> getItems(){
 		return items;
 	}
+	
 	
 	@Override
 	public int hashCode() {
