9f84a84647d691c7f9a7c60ce2e3510c5ec12a53
Arthur Tavares <arthurotavares@gmail.com>
2022-08-31 17:37:48 -0300

    Exception handling - findById

--- src/main/java/com/arthurdev/course/services/UserService.java
+++ src/main/java/com/arthurdev/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.arthurdev.course.entities.User;
 import com.arthurdev.course.repositories.UserRepository;
+import com.arthurdev.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 //registrado como componente do spring para a injecao de dependencia
@@ -22,7 +23,7 @@ public class UserService {
 
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 
 	public User insert(User obj) {
